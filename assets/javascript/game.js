4c9b271038d0a50a8a7cdd601958cef70bb9df3b
Reuben Taveras <rtaveras824@hotmail.com>
2016-07-15 22:42:42 -0400

    Heavily modified code, added awesome features to game master

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,52 +1,94 @@
 
-	var word = ["R","O","C","K"];
-	var wordguess = [];
-	var lettersguessed = [];
+	//variables
+	var wordBank = ['Robert Baratheon', 'Renly Baratheon', 'Ygritte', 'Oberyn Martell', 'Tywin Lannister', 'Khal Drogo', 'Joffrey Baratheon', 'Ned Stark', 'Rob Stark', 'Catelyn Stark'];
+	var imageBank = ['robert', 'renly', 'ygritte', 'oberyn', 'tywin', 'khal', 'joffrey', 'ned', 'rob', 'catelyn'];
+	var word = []
+	var wordGuess = [];
+	var lettersGuessed = [];
+	var gameInstance = [];
+
+	var randomNumGen = 0;
+	var imageSrc = '';
 
 	var wins = 0;
-	var numguesses = 13;
+	var numGuesses = 13;
+	var id = 0;
+	var newWordState = false;
+	var winState = false;
 
-	document.getElementById('fortouch').focus();
+	//added this for mobile functionality
+	//document.getElementById('fortouch').focus();
 
-	document.getElementById('numguesses').innerHTML = numguesses;
+	//functions
 
-	setBlankLetters();
-
+	//adds key down event to document
 	function addHandler() {
 		document.addEventListener('keydown', letterEntered);
 	}
 
+	//removes key down event to document
 	function removeHandler() {
 		document.removeEventListener('keydown', letterEntered);
 	}
 
-	addHandler();
+
 
 	//STRINGS ARE ARRAYS
 	//indexOf VERY IMPORTANT
 
+	//sets string passed in to upper case
+	function setUpperCase (param) {
+		for (var i = 0; i < param.length; i++) {
+			param[i] = param[i].toUpperCase();
+		}
+		return param;
+	}
+
+	//selects a random word from wordBank
+	function selectRandomWord () {
+		//defaults newWordState and word
+		newWordState = false;
+		word = [];
+
+		//Picks random word from wordBank, sets the img, uppercases random word, then word array is set to random word
+		randomNumGen = Math.floor(Math.random() * wordBank.length);
+		var randomWord = wordBank[randomNumGen];
+		imageSrc = "assets/images/" + imageBank[randomNumGen] + ".jpg";
+		for (var i = 0; i < randomWord.length; i++) {
+			word.push(randomWord[i].toUpperCase());
+		}
+	}
+
 	//changes blank to letters guessed
 	function letterPass () {
 		for (var i=0; i<word.length; i++) {
-			document.getElementById("characters").innerHTML += wordguess[i];
+			document.getElementById("characters").innerHTML += wordGuess[i];
 		}
 	}
 
 	//inserts blank characters
 	function setBlankLetters () {
-		//reset wordguess array and characters div
-		wordguess = [];
+		//reset wordGuess array and characters div
+		wordGuess = [];
 		document.getElementById("characters").innerHTML = '';
 		for (var i=0; i<word.length; i++) {
 			//This is the last item in array
 			if(i == word.length -1) {
 				//last underscore does not get space at the end
-				wordguess.push("_");
+				wordGuess.push("_");
+				document.getElementById("characters").innerHTML += wordGuess[i];
 			} else {
+				//this is suppose to check for spaces and add it in, but doesn't seem to work, could be bootstrap formatting
+				if (word[i] == ' ') {
+					wordGuess.push(word[i]);
+					document.getElementById("characters").innerHTML += '   ';
+					continue;
+				}
 				//each underscore gets a space
-				wordguess.push("_ ");
+				wordGuess.push("_ ");
+				document.getElementById("characters").innerHTML += wordGuess[i];
 			}
-			document.getElementById("characters").innerHTML += wordguess[i];
+			
 		}
 	}
 
@@ -66,63 +108,147 @@
 
 	//when a key is pressed
 	function letterEntered (e) {
+		//turn keyCode to letter
 		var letter = String.fromCharCode(e.keyCode);
-		var guessedcorrect = false;
+		
+		//reset characters displayed
 		document.getElementById("characters").innerHTML = '';
 
+		//check to see if letter is in word
 		var letterCorrect = word.indexOf(letter);
+		//if so, change letter in wordGuess to correct one, and continue to check if the letter appears again in word
 		if (letterCorrect !== -1){
 			while (letterCorrect !== -1) {
-				wordguess[letterCorrect] = letter;
+				wordGuess[letterCorrect] = letter;
 				letterCorrect = word.indexOf(letter, letterCorrect + 1);
 			}
 		} else {
-			var letterWrong = lettersguessed.indexOf(letter);
+			//letter is not in word
+			//check to see if you already entered letter
+			var letterWrong = lettersGuessed.indexOf(letter);
+			//if not, add letter to letters guessed then minus one number of guesses left
 			if (letterWrong == -1) {
-				lettersguessed.push(letter);
-				document.getElementById("missed").innerHTML += lettersguessed[lettersguessed.length-1] + ' ';
-				numguesses--;
-				document.getElementById("numguesses").innerHTML = numguesses;
+				lettersGuessed.push(letter);
+				document.getElementById("missed").innerHTML += lettersGuessed[lettersGuessed.length-1] + ' ';
+				numGuesses--;
+				document.getElementById("numguesses").innerHTML = numGuesses;
 			}
 		}
 
 		//if word guessed and word are equal, win, reset game
-		if (arraysEqual(wordguess, word)) {
+		if (arraysEqual(wordGuess, word)) {
 			wins += 1;
+			//winState set to win for record keeping
+			winState = 'win';
+			//make new instance of game recording variable to be displayed in game master window
+			var game = GameState(++id, winState, word, wordGuess, lettersGuessed, numGuesses);
+			//each GameState instance is added to an array to loop through
+			gameInstance.push(game);
+			//remove key down event
 			removeHandler();
+			//edit html elements to display wins and play again button
 			document.getElementById("wins").innerHTML = 'Wins: ' + wins;
-			document.getElementById("outcome").innerHTML = 'YOU WIN! <br> <button type="button" class="btn btn-default" onclick="reset();addHandler();">Play Again</button>';
+			document.getElementById("outcome").innerHTML = 'YOU WIN! <br> <button type="button" class="btn btn-default" onclick="selectRandomWord();addHandler();reset();">Play Again</button>';
+			//if not a new word from game master window, change image to word, remove them from wordBank and imageBank to prevent duplication
+			if(!newWordState) {
+				document.getElementById("picture").src = imageSrc;
+				imageBank.splice(randomNumGen, 1);
+				wordBank.splice(randomNumGen, 1);
+			}
 		}
 
 		//if you run out of guesses, reset game
-		if (numguesses == 0) {
-			wordguess = word;
-			document.getElementById("outcome").innerHTML = 'YOU LOST! <br> <button type="button" class="btn btn-default" onclick="reset();addHandler();">Play Again</button>';
+		if (numGuesses == 0) {
+			//winState set to loss for record keeping
+			winState = 'loss';
+			//make new instance of game recording variable to be displayed in game master window
+			var game = GameState(++id, winState, word, wordGuess, lettersGuessed, numGuesses);
+			//each GameState instance is added to an array to loop through
+			gameInstance.push(game);
+			//set wordGuess to word to display answer
+			wordGuess = word;
 			removeHandler();
+			//edit html elements to display play again button and loss image
+			document.getElementById("outcome").innerHTML = 'YOU LOST! <br> <button type="button" class="btn btn-default" onclick="reset();addHandler();">Play Again</button>';
+			document.getElementById("picture").src = "assets/images/loser.jpg";
+			//if not a new word from game master window, remove word from wordBank to prevent duplication
+			if (!newWordState) {
+				wordBank.splice(randomNumGen, 1);
+			}
+		}
+
+		//if no more words from wordBank, thats it. Now you can only play with new words entered
+		if (wordBank == 0) {
+			document.getElementById("outcome").innerHTML = '';
 		}
 
 		//show correct letters
 		letterPass();
 	}
 		
+	//sets word entered in game master window to word to be guessed, and newWordState to true so it doesn't mess with wordBank or imageBank
 	function newWord() {
-		var newword = document.getElementById("newword").value.toUpperCase();
+		var newword = document.getElementById('newword').value.toUpperCase();
 		var newwordarray = newword.split('');
 		word = newwordarray;
+		newWordState = true;
+		imageSrc = "assets/images/loser.jpg";
 	}
 
+	//resets important variables back to default and updated html
 	function reset() {
-		numguesses = 13;
-		lettersguessed = [];
-		wordguess = [];
-		winstate = false;
-		document.getElementById('numguesses').innerHTML = numguesses;
-		document.getElementById('missed').innerHTML = '';
-		document.getElementById('outcome').innerHTML = '';
+		numGuesses = 13;
+		lettersGuessed = [];
+		wordGuess = [];
+		document.getElementById("numguesses").innerHTML = numGuesses;
+		document.getElementById("missed").innerHTML = '';
+		document.getElementById("outcome").innerHTML = '';
 		setBlankLetters();
 	}
 
-/* Old code just for reference.
+	//make instances of each game to record variables and display them in game master window
+	function GameState(id, win, word, wordGuess, lettersGuessed, numGuesses) {
+		var gameState  = {
+			id: id,
+			win: win,
+			word: word,
+			wordGuess: wordGuess,
+			lettersGuessed: lettersGuessed,
+			numGuesses: numGuesses,
+		}
+
+		return gameState;
+	}
+
+	//loops through gameInstance array, display each game, and their stats
+	function displayGameInstances () {
+		document.getElementById("content").innerHTML = '';
+		for (var i = 0; i < gameInstance.length; i++) {
+			var word = gameInstance[i].word.join('');
+			var userWord = gameInstance[i].wordGuess.join('');
+			var content = "<strong>id:</strong> " + gameInstance[i].id + " | <strong>outcome:</strong> " + gameInstance[i].win + " | <strong>word:</strong> " + word + " | <strong>user word:</strong> " + userWord + " | <strong>letters guessed:</strong> " + gameInstance[i].lettersGuessed + " | <strong>guesses left:</strong> " + gameInstance[i].numGuesses + "<hr>";
+			document.getElementById("content").innerHTML += content;
+		}
+	}
+
+	//code take from stack overflow to scroll to certain part of webpage on click
+	$("#scrollBtn").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#theGame").offset().top},
+        'slow');
+	});
+
+
+	//initializes game, what shows when its your first time on the page
+	document.getElementById('numguesses').innerHTML = numGuesses;
+
+	selectRandomWord();
+
+	setBlankLetters();
+
+	addHandler();
+
+/* Old code just for reference. Heavily modified after learning about indexOf array method
 		for (var i=0; i<word.length; i++) {
 			//through entire word length
 			if(word[i] == letter) {
